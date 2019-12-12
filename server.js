c1c6d806437b98e66cda308c38c1241b47edc684
Zalbani <pro.pierson.alban@gmail.com>
2019-12-12 12:58:38 +0100

    Move console.log

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const localConfig = require('./config/local.json');
 
 
 server.get('/', function (req, res) {
-    console.log("Serveur lancer sur : Localhost:3000");
     res.send("passe");
 });
-
+console.log("Serveur lancer sur : Localhost:3000");
 server.listen(localConfig.SERVER_PORT);
