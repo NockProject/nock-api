6334a26a0892793758835cddf21609e932d73d6f
Zalbani <pro.pierson.alban@gmail.com>
2019-12-13 09:31:07 +0100

    First try for deploy

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-'use strict';
+const http = require('http');
+const app = require('./app');
 
-const server = require('express')();
-const localConfig = require('./config/local.json');
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-server.get('/', function (req, res) {
-    res.send("passe");
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
-console.log("Serveur lancer sur : Localhost:3000");
-server.listen(localConfig.SERVER_PORT);
+
+server.listen(port);
