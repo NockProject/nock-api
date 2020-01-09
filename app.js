0a8ba4fe4ffc122c1e94ee771aac4b4f03638254
Zalbani <pro.pierson.alban@gmail.com>
2020-01-09 16:08:40 +0100

    Ca marche ?

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ mongoose.connect(process.env.DB_URI,
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, XMLHttpRequest');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, XMLHttpRequest');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
 app.use(cors);
 app.use(bodyParser.json());
 
