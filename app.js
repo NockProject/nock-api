820157dc65e6fc13efeafded26e7eb8455b96601
Zalbani <pro.pierson.alban@gmail.com>
2020-01-09 10:23:23 +0100

    Add postman exports

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use('/api/comm', commentRoutes);
 app.use('/api/building', buildingRoutes);
 
 require('./CRON/moderationInsults');
-// require('./quickStart');
+// require('./config/quickStart');
 
 module.exports = app;
