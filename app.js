c1ae226a3fa2a124254343336dafc392944b0243
Zalbani <pro.pierson.alban@gmail.com>
2020-01-09 16:11:55 +0100

    try

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 //     next();
 // });
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
